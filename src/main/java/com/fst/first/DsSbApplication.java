e53a845be8b8854f0e2e6d92d3ffad2673e4e27c
maria94-crypto <mariem.baalouch@etudiant-fst.utm.tn>
2025-03-28 03:19:09 +0100

    first commit

---
+++ src/main/java/com/fst/first/DsSbApplication.java
@@ -0,0 +1,13 @@
+package com.fst.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsSbApplication.class, args);
+	}
+
+}
